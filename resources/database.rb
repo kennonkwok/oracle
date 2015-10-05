2e7fec2682f32f317dfd7fc4cfbf95e8627b2058
Kennon Kwok <kennon@getchef.com>
2015-10-05 14:49:48 -0700

    add db delete action, add load_current_value code to check oratab

--- resources/database.rb
+++ resources/database.rb
@@ -3,9 +3,16 @@ property :emconfiguration, String, default: 'LOCAL'
 property :templatename, String, default: 'General_Purpose.dbc'
 property :syspassword, String, default: node[:oracle][:rdbms][:sys_pw]
 property :systempassword, String, default: node[:oracle][:rdbms][:system_pw]
+property :databases, Array, default: []
 
 load_current_value do
-  #check if db exists
+  if ::File.exist?('/etc/oratab')
+    oratab = ::File.open('/etc/oratab', 'r')
+    oratab.each_line do |line|
+      next if line.chomp! =~ /^#/
+      databases << line.split(':').first
+    end
+  end
 end
 
 action :create do
@@ -20,9 +27,21 @@ action :create do
     action :run
     user 'oracle'
     environment node[:oracle][:rdbms][:env]
+    not_if do
+      databases.include? dbname
+    end
   end
 end
 
-action :destroy do
-  #dbca -silent -deleteDatabase...
+action :delete do
+  execute "dbca delete #{dbname}" do
+    command "dbca -silent -deleteDatabase \
+     -sourceDB #{dbname}"
+    action :run
+    user 'oracle'
+    environment node[:oracle][:rdbms][:env]
+    only_if do
+      databases.include? dbname
+    end
+  end
 end
